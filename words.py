63085971a14d751d865b2d6e52e0341187f94dad
Venkata _Bhanu_Teja _konijeti <154120763+VenkataBhanuTejaKonijeti@users.noreply.github.com>
2024-09-01 19:32:52 +0530

    Update words.py

--- words.py
+++ words.py
@@ -1,4 +1,4 @@
-words = words = [
+words =[
     'apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi',
     'lemon', 'mango', 'nectarine', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry',
     'tangerine', 'ugli', 'vanilla', 'watermelon', 'xigua', 'yellowfruit', 'zucchini', 'apricot',
